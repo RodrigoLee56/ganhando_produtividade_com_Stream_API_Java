2ba15c36dbba6ecdbcb42c52b1996df3332a0997
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-04-12 01:38:10 -0400

    subindo o projeto consumer, exemplo 3

--- consumer/src/consumer/ConsumerExample.java
+++ consumer/src/consumer/ConsumerExample.java
@@ -2,23 +2,12 @@ package consumer;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class ConsumerExample {
 
 	public static void main(String[] args) {
 		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
-
-		// Usar o Consumer com uma classe anônima para imprimir números pares
-		Consumer<Integer> imprimirNumeroPar = numero -> {
-			if (numero % 2 == 0) {
-				System.out.println(numero);
-			}
-		};
-
-		// Usar o Consumer para imprimir números pares da lista
-		numeros.stream().forEach(imprimirNumeroPar);
-
+		numeros.stream().filter(numero -> numero % 2 == 0).forEach(System.out::println);
 	}
 
 }
