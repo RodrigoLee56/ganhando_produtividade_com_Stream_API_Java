7503edf803e5243537123a99b35c24af44f26f3a
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-04-13 10:18:44 -0400

    subindo o projeto function, exemplo 2

--- function/src/function/FunctionExample.java
+++ function/src/function/FunctionExample.java
@@ -1,31 +1,20 @@
 package function;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FunctionExample {
 
 	public static void main(String[] args) {
 		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
-		Function<Integer, Integer> dobrar = new Function<Integer, Integer>() {
-
-			@Override
-			public Integer apply(Integer numero) {
-				return numero * 2;
-			}
-		};
-
-		List<Integer> numerosDobrados = new ArrayList<>();
-		for (Integer numero : numeros) {
-			numerosDobrados.add(dobrar.apply(numero));
-		}
-
-		for (Integer numerosDobrado : numerosDobrados) {
-			System.out.println(numerosDobrado);
-		}
+		Function<Integer, Integer> dobrar = numero -> numero * 2;
+		
+		List<Integer> numerosDobrados = numeros.stream().map(dobrar).collect(Collectors.toList());
+		
+		numerosDobrados.forEach(System.out::println);
 	}
 
 }
