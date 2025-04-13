2ab0d08ecff4931035d33972090758cd1bea368e
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-04-13 10:36:59 -0400

    subindo o projeto predicate, exemplo 2

--- predicate/src/predicate/PredicateExample.java
+++ predicate/src/predicate/PredicateExample.java
@@ -1,33 +1,20 @@
 package predicate;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class PredicateExample {
 
 	public static void main(String[] args) {
 		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-		Predicate<Integer> isPar = new Predicate<Integer>() {
+		Predicate<Integer> isPar = numero -> numero % 2 == 0;
 
-			@Override
-			public boolean test(Integer numero) {
-				return numero % 2 == 0;
-			}
-		};
+		List<Integer> numerosPares = numeros.stream().filter(isPar).collect(Collectors.toList());
 
-		List<Integer> numerosPares = new ArrayList<>();
-		for (Integer numero : numeros) {
-			if (isPar.test(numero)) {
-				numerosPares.add(numero);
-			}
-		}
-
-		for (Integer numeroPare : numerosPares) {
-			System.out.println(numeroPare);
-		}
+		numerosPares.forEach(System.out::println);
 	}
 
 }
